171fc64d77cd4745bb16d0e20dac6d0165823189
Selwyn Ang <selwynang.01@gmail.com>
2024-01-13 18:19:25 +0800

    Solved Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    toReturn = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (i = 0; i < num; i++) {
+        toReturn += str; 
+    }
 
+    return toReturn;
 };
 
 // Do not edit below this line
