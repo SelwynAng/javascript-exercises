f543a1ac392683711fdee269c7ab3fe10a81acec
Selwyn Ang <selwynang.01@gmail.com>
2024-01-13 18:58:21 +0800

    Solved Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (typeof numOne != "number" || typeof numTwo != "number") {
+        return "ERROR";
+    }
 
+    if (numOne < 0 || numTwo < 0) {
+        return "ERROR";
+    }
+
+    sum = 0;
+
+    if (numOne <= numTwo) {
+        for (i = numOne; i < numTwo + 1; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = numTwo; i < numOne + 1; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
