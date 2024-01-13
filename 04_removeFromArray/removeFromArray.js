66a36bcd85f8bd96db150671e7d6cedfac912f5c
Selwyn Ang <selwynang.01@gmail.com>
2024-01-13 18:48:24 +0800

    Solved Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    arr = args[0];
 
+    toRemove = []
+    if (args.length > 1) {
+        for (i = 1; i < args.length; i++) {
+            toRemove.push(args[i]);
+        }
+    }
+
+    toReturn = []
+    for (i = 0; i < arr.length; i++) {
+        if (toRemove.includes(arr[i])) {
+            continue;
+        }
+        toReturn.push(arr[i]);
+    }
+
+    return toReturn;
 };
 
 // Do not edit below this line
